2d5b292bdbdbbd279107d5f9fc5f31dccdab3567
RamdanAG <121142467+RamdanAG@users.noreply.github.com>
2024-11-30 23:08:35 -0800

    fix config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
-from dotenv import load_dotenv
-import os
+GEMINI_API_KEY = ""
+GEMINI_API_URL = ""
 
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_API_URL = 
-
-VOICEVOX_BASE_URL = 
-DEFAULT_SPEAKER = 1
+VOICEVOX_BASE_URL = "" 
+DEFAULT_SPEAKER = 
